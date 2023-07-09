8b61d72cf6ece1cb6859fca55bd8d2b320410edc
HQiuzi <494755390@qq.com>
2023-07-09 14:48:21 +0800

    npc 闲置浮动脚本

---
+++ Assets/Script/IdleMove.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleMove : MonoBehaviour
+{
+    public float maxRange = 0.2f;
+    public float speed = 1.0f;
+
+    private float originY;
+
+    private void Start()
+    {
+        originY = transform.position.y;
+    }
+
+    void Update()
+    {
+        float amount = Mathf.Sin(speed * Time.time) * maxRange;
+        transform.position = new Vector3(transform.position.x, originY + amount, transform.position.z);
+    }
+}
